fd0a6708a639351f12704ac264bf6adf9aa6910e
Insopitus <jessefor@hotmail.com>
2023-12-29 10:08:47 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 // import { threeMinifier } from "@yushijinhun/three-minifier-rollup";
 // import visualizer from 'rollup-plugin-visualizer'
 export default defineConfig({
+	base:'./'
 	plugins: [
 		// { ...threeMinifier(), enforce: "pre" }, // <=== Add plugin here
 		// visualizer()
